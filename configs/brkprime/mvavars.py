efef4460688d690d4f888db2eb59127cebae3b41
Irene Andreou <irene.andreou@cern.ch>
2024-10-21 13:42:39 +0100

    first commit

--- configs/brkprime/mvavars.py
+++ configs/brkprime/mvavars.py
@@ -9,12 +9,23 @@ KINEMATIC_VARS = [
 
 # Use here only variables available in real data.
 MVA_SCALAR_VARS = [
-'Z_mass',
-'Z_pt',
-'wt',
-'n_jets',
-'n_deepbjets', 
-'mjj',
+  'decayMode_1',
+  'decayMode_2',
+  'jpt_pt_1',
+  'jpt_pt_2',
+  'pt_1',
+  'pt_2',
+  'eta_1',
+  'eta_2',
+  'charge_1',
+  'charge_2',
+  'n_jets',
+  'n_prebjets',
+# 'Z_mass',
+# 'Z_pt',
+# 'n_jets',
+# 'n_deepbjets', 
+# 'mjj',
 ]
 
 # Mutual information regularization targets
@@ -23,12 +34,23 @@ MI_VARS = [
 
 # Variables we use with name replacement, need to be found in both MC and DATA
 NEW_VARS = [
-'Z_mass',
-'Z_pt',
-'wt',
-'n_jets',
-'n_deepbjets', 
-'mjj',
+  'decayMode_1',
+  'decayMode_2',
+  'jpt_pt_1',
+  'jpt_pt_2',
+  'pt_1',
+  'pt_2',
+  'eta_1',
+  'eta_2',
+  'charge_1',
+  'charge_2',
+  'n_jets',
+  'n_prebjets',
+# 'Z_mass',
+# 'Z_pt',
+# 'n_jets',
+# 'n_deepbjets', 
+# 'mjj',
 ]
 
 # Variables we read out from the root files (regular expressions supported here)
